1a1cc1d7824cdf61a0ad0cdd617bcaf9c8d9edb9
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-08-08 14:42:22 +0530

    Saving into database b y iterating and processing each.

--- index.js
+++ index.js
@@ -5,75 +5,144 @@ const Stats = mongoose.model("Stats");
 const cron = require("node-cron");
 const uriFetch_markets = "https://bittrex.com/api/v1.1/public/getmarkets";
 const uri = "https://bittrex.com/Api/v2.0/pub/market/GetLatestTick";
+const curntDate = new Date();
+const hour = curntDate.getHours();
 
 const market_options = {
   uri: uriFetch_markets,
   method: "GET",
   json: true
 };
-function save_it(formatted_data){
-  const curntDate = new Date();
-  const hour = curntDate.getHours();
-
- Stats.update({marketName:formatted_data.marketName,hour:hour},{
-  $setOnInsert:{
-      hour: hour,
-      marketName: formatted_data.marketName,
-      open:formatted_data.O,
-      close:formatted_data.C,
-      high:formatted_data.H,
-      low:formatted_data.L ,
-      total_volume:formatted_data.V,
-      count:1,
-      all_data: [{
-        open:formatted_data.O,
-        close:formatted_data.C,
-        high:formatted_data.H,
-        low:formatted_data.L ,
-        volume: formatted_data.V,
-        data_timestamp:formatted_data.T
-      }]
-  }
- }) 
-}
-
-function process_it() {
-  rp(market_options)
+function save_it(formatted_data) {
+  return Stats.update(
+    { marketName: formatted_data.marketName, hour: hour },
+    {
+      $inc: {
+        count: 1,
+        total_volume: formatted_data.result[0].V
+      },
+      $max: {
+        high: formatted_data.result[0].H
+      },
+      $min: {
+        low: formatted_data.result[0].L
+      },
+      $set: {
+        close: formatted_data.result[0].C
+      },
+      $push: {
+        all_data: {
+          open: formatted_data.result[0].O,
+          close: formatted_data.result[0].C,
+          high: formatted_data.result[0].H,
+          low: formatted_data.result[0].L,
+          volume: formatted_data.result[0].V,
+          data_timestamp: formatted_data.result[0].T
+        }
+      }
+    },
+    { upsert: false }
+  )
+    .exec()
     .then(data => {
-      // in This  method i'm unable to get market names.
-      //  let promiseArr = data.result.map((i)=>rp(`${uri}?marketName=${i.MarketName}&tickInterval=oneMin&_=${Date.now()}`));
-      //     Promise.all(promiseArr).then(data=>{
-      //       console.log(data);
-      //     }).catch(error_tick=>{
-      //       throw error_tick;
-      //     });
-
-      data.result.forEach(i => {
-        rp(
-          `${uri}?marketName=${
-            i.MarketName
-          }&tickInterval=oneMin&_=${Date.now()}`
-        )
-          .then(tick_data => {
-            let tick_formatted = {
-              ...JSON.parse(tick_data),
-              marketName: i.MarketName
-            };
-            save_it(tick_formatted);
-            // console.log(tick_formatted)
-            //save it here in proper format
-          })
-          .catch(error_tick => {
-            console.log(error_tick);
+      if (data.nModified == 0) {
+        try {
+          return Stats.insertMany({
+            hour: hour,
+            marketName: formatted_data.marketName,
+            open: formatted_data.result[0].O,
+            close: formatted_data.result[0].C,
+            high: formatted_data.result[0].H,
+            low: formatted_data.result[0].L,
+            total_volume: formatted_data.result[0].V,
+            count: 1,
+            all_data: [
+              {
+                open: formatted_data.result[0].O,
+                close: formatted_data.result[0].C,
+                high: formatted_data.result[0].H,
+                low: formatted_data.result[0].L,
+                volume: formatted_data.result[0].V,
+                data_timestamp: formatted_data.result[0].T
+              }
+            ]
           });
-      });
-    })
-    .catch(error_market => {
-      throw error_market;
+        } catch (error_insert) {
+          return Promise.reject(Error(error_insert));
+        }
+      } else {
+        return Promise.resolve(data);
+      }
     });
 }
 
+function process_it() {
+  let promiseArr = [];
+  return new Promise((resolve, reject) => {
+    rp(market_options)
+      .then(data => {
+        // in This  method i'm unable to get market names.
+        //  let promiseArr = data.result.map((i)=>rp(`${uri}?marketName=${i.MarketName}&tickInterval=oneMin&_=${Date.now()}`));
+        //     Promise.all(promiseArr).then(data=>{
+        //       console.log(data);
+        //     }).catch(error_tick=>{
+        //       throw error_tick;
+        //     });
+
+        data.result.forEach(i => {
+          rp(
+            `${uri}?marketName=${
+              i.MarketName
+            }&tickInterval=oneMin&_=${Date.now()}`
+          )
+            .then(tick_data => {
+              let tick_formatted = {
+                ...JSON.parse(tick_data),
+                marketName: i.MarketName
+              };
+              Stats.findOne({
+                marketName: tick_formatted.marketName,
+                hour: hour,
+                "all_data.data_timestamp": tick_formatted.result[0].T
+              })
+                .select({ "all_data.$": 1 })
+                .exec()
+                .then(found_data => {
+                  if (!found_data || !found_data.all_data) {
+                    promiseArr.push(save_it(tick_formatted));
+                  }
+                })
+                .catch(error_finding => {
+                  return reject(Error(error_finding));
+                });
+              if (data.result.length == promiseArr.length) {
+                Promise.all(promiseArr)
+                  .then(data_saved => {
+                    return resolve(data_saved);
+                  })
+                  .catch(error_saving => {
+                    return reject(Error(error_saving));
+                  });
+              }
+            })
+            .catch(error_tick => {
+              return reject(Error(error_tick));
+            });
+        });
+      })
+      .catch(error_market => {
+        return reject(Error(error_market));
+      });
+  });
+}
+
 cron.schedule("* * * * *", () => {
-  process_it();
-  console.log("Done Once!")
+  process_it()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  console.log("Triggering!");
 });
