c12dcaf944f758cc0c3933dc07d162b0d2ffcc71
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-08-09 11:00:59 +0530

    lets not create _id for saving the data

--- models/Stats.js
+++ models/Stats.js
@@ -1,42 +1,43 @@
 const mongoose = require("mongoose");
 
-var MinuteSchema = mongoose.Schema({
-  open: Number,
-  close: Number,
-  high: Number,
-  low: Number,
-  volume: Number,
-  data_timestamp:Date
-});
-var StatSchema = mongoose.Schema(
+var MinuteSchema = mongoose.Schema(
   {
-    hour: Number,
-    marketName: String,
-    open: {
-      type: Number,
-      default: 0
-    },
-    close: {
-      type: Number,
-      default: 0
-    },
-    high: {
-      type: Number,
-      default: 0
-    },
-    low: {
-      type: Number,
-      default: 0
-    },
-    total_volume:{
-        type: Number,
-        default: 0
-    },
-    count: {
-      type: Number,
-      default: 0
-    },
-    all_data: [MinuteSchema]
-  }
+    open: Number,
+    close: Number,
+    high: Number,
+    low: Number,
+    volume: Number,
+    data_timestamp: Date
+  },
+  { _id: false }
 );
+var StatSchema = mongoose.Schema({
+  hour: Number,
+  marketName: String,
+  open: {
+    type: Number,
+    default: 0
+  },
+  close: {
+    type: Number,
+    default: 0
+  },
+  high: {
+    type: Number,
+    default: 0
+  },
+  low: {
+    type: Number,
+    default: 0
+  },
+  total_volume: {
+    type: Number,
+    default: 0
+  },
+  count: {
+    type: Number,
+    default: 0
+  },
+  all_data: [MinuteSchema]
+});
 module.exports = { StatSchema };
