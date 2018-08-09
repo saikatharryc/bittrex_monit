19c8b242bdda2d7e22f629aaed4a35099ad5c101
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-08-09 12:34:14 +0530

    Config now based on ENV variables

--- config/config.js
+++ config/config.js
@@ -1,24 +1,67 @@
+const Joi = require("joi");
+
+const envs = process.env;
 let config = {};
-config.db = {
-  uri: "mongodb://saikat_t:saikat95@ds135866.mlab.com:35866/taskerdb",
-  options: {
-    // replicaSet: "rs0",
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 500, // Reconnect every 500ms
-    keepAlive: 120,
-    autoReconnect: true,
-    poolSize: 20,
-    reconnectTries: Number.MAX_VALUE,
-    // sets the delay between every retry (milliseconds)
-    reconnectInterval: 1000
+
+const config_schema = Joi.object().keys({
+  db: {
+    uri: Joi.string().required(),
+    options: {
+      reconnectTries: Joi.number().required(),
+      reconnectInterval: Joi.number().required(),
+      keepAlive: Joi.number().required(),
+      autoReconnect: Joi.boolean().required(),
+      poolSize: Joi.number().required()
+    }
+  },
+  smtp_credentials :{
+    smtp_host: Joi.string().required(),
+    port:Joi.number().required(),
+    user: Joi.string().required(),
+    pass: Joi.string().required(),
+    send_to:Joi.string().required(),
+    from_data: Joi.string().required()
+  },
+  req_uri = {
+    market: Joi.string().required(),
+    latest_tick: Joi.string().required()
   }
-};
-config.smtp_credentials = {
-  smtp_host: "email.kiot.io",
-  port: 587,
-  user: "saikat@kiot.io",
-  pass: "@Saikat1234",
-  from_data: '"Some*** Administration" <saikat@kiot.io>'
-};
+});
+
+if (process.env.ENV == "prod") {
+  config.db = {
+    uri: envs.DB_URI,
+    options: {
+      // replicaSet: "rs0",
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 500, // Reconnect every 500ms
+      keepAlive: 120,
+      autoReconnect: true,
+      poolSize: 20
+    }
+  };
+  config.smtp_credentials = {
+    smtp_host: envs.SMTP_HOST,
+    port: envs.SMTP_PORT,
+    user: envs.SMTP_USER,
+    pass: envs.SMTP_PASS,
+    send_to:envs.SEND_TO,
+    from_data: '"Some*** Administration" <saikat@kiot.io>'
+  };
+  config.req_uri = {
+    market: envs.MARKET,
+    latest_tick: envs.LATEST_TICK
+  };
+} else {
+  //do something else
+}
+//Just to make sure  Every Item is there.
+Joi.validate(config, config_schema, err => {
+  if (err) {
+    throw err;
+  }
+});
+// in runtime lets not allow anything to change value of any key[as much as possible]
 Object.freeze(config);
+
 module.exports = config;
